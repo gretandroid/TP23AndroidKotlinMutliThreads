045cae754adff5dce22fb5a2c76495982435918f
cheroliv <40507040+cheroliv@users.noreply.github.com>
2022-04-20 10:34:19 +0200

    jouons avec des threads

--- app/src/main/java/com/example/kotlin/mutlithreads/MainActivity.kt
+++ app/src/main/java/com/example/kotlin/mutlithreads/MainActivity.kt
@@ -16,10 +16,38 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onClickButtonEvent(view: View) {
-        binding.threadOutputView.text=""
-        display("code du thread UI")
-        display("code du thread UI")
-        display("code du thread UI")
+        // Traitement dans le thread UI
+//        //old fashion
+//        val runnable = Runnable { display("code du runnable deprecated") }
+//        val handler = Handler()
+//        handler.post(runnable)
+
+//        //placer du code à executer dans la pile du main thread
+//        // qui sera executé en dernier par rapport au flow synchrone
+//        val mainExecutor = ContextCompat.getMainExecutor(this)
+//        mainExecutor.execute { display("code du runnable") }
+
+//        // plus besoin
+//        binding.threadOutputView.text = ""
+//        display("code du thread UI")
+//        display("code du thread UI")
+//        display("code du thread UI")
+        //Fin du Traitement dans le thread UI
+
+        // création d'un thread qui est interdit
+        // impossibilité de communiquer avec le UI thread
+        // à partir d'un thread créé manuellement
+        val runnable = Runnable {
+            display("avant la boucle")
+            for (i in 1..10) {
+                display("loop : $i")
+            }
+            display("après la boucle")
+        }
+
+
+        val thread = Thread(runnable)
+        thread.start()
     }
 
     fun display(msg: String) {
